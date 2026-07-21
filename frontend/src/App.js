1f770fb3e7f3d7f0b7867cb75435c86147413a6f
bjmashele <bjmashele@gmail.com>
2026-07-21 01:04:23 -0500

    Fix docker-compose error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React JS Client
+          React JS Client !!!!!!
         </a>
       </header>
     </div>
